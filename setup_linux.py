f173a4f8484a806da882f28031b0a291d07b35d4
Jinwoo Lee <blackbox0209@gmail.com>
2024-07-10 22:33:18 +0900

    change build type to appimage

--- setup_linux.py
+++ setup_linux.py
@@ -3,7 +3,10 @@ from cx_Freeze import setup, Executable
 # Dependencies are automatically detected, but they might need fine-tuning.
 build_exe_options = {
     "excludes": [],
-    "includes": ["queue", "Xlib.ext.xfixes", "Xlib.ext.xinput", "Xlib.ext.damage", "Xlib.ext.res"],
+    "includes": ["queue", "Xlib.ext.xfixes", "Xlib.ext.xinput", "Xlib.ext.damage", "Xlib.ext.res", "System",
+      "System.IO.Ports", "_scproxy", "_wmi", "defusedxml", "gi", "gi.repository", "msvcrt", "nturl2path", "numpy",
+      "numpy.typing", "packaging.version", "re.IGNORECASE", "typing_extensions"
+    ],
     "packages": [],
     "include_files": [
       "FlooCastApp.gif",
@@ -15,10 +18,20 @@ build_exe_options = {
     ]
 }
 
+executables = [
+    Executable(
+        "main.py",
+        base="gui",
+        icon="FlooCastApp.ico",
+        target_name="FlooCast",
+        shortcut_name="FlooCast",
+    )
+]
+
 setup(
     name="FlooCast",
     version="1.0.9",
     description="FlooGoo FMA120 Configurator",
+    executables=executables,
     options={"build_exe": build_exe_options},
-    executables=[Executable("main.py", base="gui")],
 )
